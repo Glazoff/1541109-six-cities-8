4306e85d11e1368c77158c5f339799d032848632
Женя <84893786+Glazoff@users.noreply.github.com>
2021-10-07 23:33:13 +0300

    Добавил маршруты в приложение

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,36 @@
-import MainPageScreen from '../main-page/main-page';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import {AppScreenProps} from '../../types/types';
+import {AppRoute} from '../../const';
+
+import MainPageScreen from '../main-page/main-page';
+import SignInScreen from '../sign-in-page/sign-in-page';
+import FavoritesPageScree from '../favorites-page/favorites-page';
+import RoomOfferScreen from '../room-offer-page/room-offer-page';
+import Error404Screen from '../error-404-page/error-404-page';
 
 
 function App({offerCount} : AppScreenProps): JSX.Element {
-  return <MainPageScreen offerCount={offerCount}/>;
+  return  (
+    <BrowserRouter>
+      <Switch>
+        <Route  path={AppRoute.Main} exact>
+          <MainPageScreen  offerCount={offerCount}/>
+        </Route>
+        <Route path={AppRoute.SignIn} exact>
+          <SignInScreen />
+        </Route>
+        <Route path={AppRoute.Favorites} exact>
+          <FavoritesPageScree/>
+        </Route>
+        <Route path={AppRoute.Room} exact>
+          <RoomOfferScreen/>
+        </Route>
+        <Route>
+          <Error404Screen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>);
 }
 
 export default App;
